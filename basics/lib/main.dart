0ce67ee70ade62d20cfd21f9fa538ba6e1fa6e13
Gayathri Suresh <gayathrisuresh150501@gmail.com>
2024-03-29 00:18:49 +0530

    create custom widgets

---
+++ basics/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:basics/gradient_container.dart';
+
+void main() {
+//provided by flutter framework: starts the flutter application and displays the UI screen
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+}
